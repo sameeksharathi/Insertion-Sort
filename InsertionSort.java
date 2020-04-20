696f93a7314916b62bbaff2f717b7a0fb11654e2
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-04-20 18:49:52 +0530

    Created Insertion Sort class

---
+++ InsertionSort.java
@@ -0,0 +1,20 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int j ;
+        int k ;
+        int arr[] = {30, 60, 10, 20, 40, 50};
+        for(int i =1;i<arr.length;i++) {
+            j = arr[i];
+            k = i - 1;
+            while (k >= 0 && arr[k] > j) {
+                arr[k + 1] = arr[k];
+                k = k - 1;
+            }
+            arr[k + 1] = j;
+        }
+        for(int i = 0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
